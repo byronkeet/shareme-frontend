076008978af5dcff958ac1b55b772e3c2a925fbd
Byron Keet <keetbis@gmail.com>
2022-12-07 13:46:35 +0200

    Added Feed and Spinner components

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { client } from '../client';
+import { feedQuery, searchQuery } from '../utils/data';
+import MasonryLayout from './MasonryLayout';
+import Spinner from './Spinner';
 
 const Feed = () => {
-  return (
-	<div>Feed</div>
-  )
+	const [pins, setPins] = useState();
+	const [loading, setLoading] = useState(false);
+	const { categoryId } = useParams();
+
+	useEffect(() => {
+		setLoading(true);
+		let query;
+		if (categoryId) {
+			query = searchQuery(categoryId);
+		} else {
+			query = feedQuery;
+		}
+		client.fetch(query).then((data) => {
+			setPins(data);
+		});
+		setLoading(false);
+	}, [categoryId]);
+
+	const ideaName = categoryId || 'new';
+
+	if (loading) {
+		return (
+			<Spinner message={`We are adding ${ideaName} ideas to your feed!`} />
+		);
+	}
+
+	return (
+		<div>
+			{pins && <MasonryLayout pins={pins} />}
+		</div>
+	);
 }
 
-export default Feed
+export default Feed;
