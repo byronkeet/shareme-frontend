d299985c52b5c54ed778d8ec6d1c0df9cea7a2cb
Byron Keet <keetbis@gmail.com>
2022-12-07 15:57:37 +0200

    Added Masonry Layout component

--- src/components/MasonryLayout.js
+++ src/components/MasonryLayout.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import Masonry from 'react-masonry-css';
+import Pin from './Pin';
 
-const MasonryLayout = () => {
-  return (
-	<div>MasonryLayout</div>
-  )
+const breakpointColumns = {
+	default: 4,
+	3000: 6,
+	2000: 5,
+	1200: 3,
+	1000: 2,
+	500: 1
+}
+
+const MasonryLayout = ({ pins }) => {
+	return (
+		<Masonry className='flex animate-slide-fwd' breakpointCols={breakpointColumns}>
+			{pins.map((pin) => <Pin key={pin._id} pin={pin} className='w-max' />)}
+		</Masonry>
+	)
 }
 
 export default MasonryLayout
