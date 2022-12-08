0954c2d3d643426927edef6d6c9a898547d4c046
Byron Keet <keetbis@gmail.com>
2022-12-08 17:57:21 +0200

    Fixed url showing in destination on pin component

--- src/components/Pin.jsx
+++ src/components/Pin.jsx
@@ -100,7 +100,7 @@ const Pin = ({ pin }) => {
 											rel='noreferrer'
 										>
 											<BsFillArrowUpRightCircleFill/>
-											{destination.length > 20 ? destination.slice(8,20) : destination.slice(8)}
+											{destination.length > 15 ? `${destination.slice(0,15)}...` : destination}
 										</a>
 									)}
 									{postedBy?._id === user?.sub && (
